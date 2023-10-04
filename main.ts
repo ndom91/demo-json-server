b3b1a1d4deefd249f8426a70fa2b66ec9c644260
ndom91 <yo@ndo.dev>
2023-10-04 14:54:30 +0200

    feat: initial deno oak api server

---
+++ main.ts
@@ -0,0 +1,19 @@
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
+
+const port = 8009;
+const app = new Application();
+
+const router = new Router();
+
+router.get('/', (ctx) => {
+  ctx.response.body = 'Received a GET HTTP method';
+});
+
+app.use(router.allowedMethods());
+app.use(router.routes());
+
+app.addEventListener('listen', () => {
+  console.log(`Listening on: localhost:${port}`);
+});
+
+await app.listen({ port });
